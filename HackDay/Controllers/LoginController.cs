c6cbdea6bb4bfdad370b7a21340f69e39397ba40
Dipakkumar Rohit <dipakkumar.rohit@numerator.com>
2019-07-11 12:03:48 +0530

    login page form validation and resirect to appropriate page code

--- HackDay/Controllers/LoginController.cs
+++ HackDay/Controllers/LoginController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HackDay.Models
 
 namespace HackDay.Controllers
 {
@@ -11,6 +12,19 @@ namespace HackDay.Controllers
         // GET: Login
         public ActionResult Login()
         {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LoginModel model)
+        {
+            if(model.UserName == "user" && model.Password == "user" )
+            {
+                return RedirectToAction();
+            }
+            else if(model.UserName == "admin" && model.Password == "admin")
+            {
+                return RedirectToAction();
+            }
             return View();
         }
     }
